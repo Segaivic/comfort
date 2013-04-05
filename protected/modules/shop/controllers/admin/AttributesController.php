67c312879cba3ac67c1eef30bcc19ebd5a95646a
Segaivic <sovremenny@live.ru>
2013-04-05 11:25:38 +0600

    msg

--- protected/modules/shop/controllers/admin/AttributesController.php
+++ protected/modules/shop/controllers/admin/AttributesController.php
@@ -1,6 +1,6 @@
 <?php
 
-class BrandsController extends Controller
+class AttributesController extends Controller
 {
     public $layout = 'application.modules.admin.layouts.admin';
 
@@ -11,11 +11,11 @@ class BrandsController extends Controller
         );
     }
 
- function accessRules()
+    function accessRules()
     {
         return array(
             array('allow',  // allow all users to perform 'index' and 'view' actions
-                'actions'=>array('index , create'),
+                'actions'=>array('index' , 'create','titles','updateattribute','deletetitle','UpdatePositions'),
                 'users'=>Yii::app()->getModule('user')->getAdmins(),
             ),
 
@@ -25,17 +25,108 @@ class BrandsController extends Controller
         );
     }
 
-    public function actionIndex()
+
+    public function actionIndex($id)
 	{
-        $root=SCategories::model()->findByPk(1);
-        $model=$root->children()->findAll();
-        $this->render('index' , array('model'=>$model));
+      $model = SProducts::model()->findByPk($id);
+      $attributes = SAttributeTitles::model()->with('attrs')->findAll(array(
+        'condition' => 'category_id = :category_id',
+        'order' => 'pos ASC',
+        'params' => array(':category_id'=>$model->category_id),
+        ));
+
+      $this->render('index', array(
+                    'model' => $model,
+                    'attributes' => $attributes,
+      ));
 	}
 
+    public function actionTitles($id){
+        $model = SAttributeTitles::itemsList($id);
+        $category = SCategories::model()->findByPk($id);
 
-    public function loadModel($id)
+        $attr = new SAttributeTitles();
+        if(isset($_POST['SAttributeTitles']))
+        {
+            $attr->attributes=$_POST['SAttributeTitles'];
+            $attr->category_id = $id;
+            $attr->pos = SAttributeTitles::maxPosition() + 1;
+
+            if($attr->save())
+                Yii::app()->user->setFlash('success', "Успешно добавлено");
+            $this->redirect($id);
+        }
+        $this->render('titles' , array(
+                                    'model' => $model ,
+                                    'category' => $category ,
+                                    'attr' => $attr)
+        );
+    }
+
+    public function actionUpdateAttribute($id) {
+
+        $attr = SAttributeTitles::model()->with('category')->findByPk($id);
+
+
+        if(isset($_POST['SAttributeTitles']))
+        {
+
+            $attr->attributes=$_POST['SAttributeTitles'];
+            if($attr->save())
+                Yii::app()->user->setFlash('success', "Успешно отредактировано");
+            $this->redirect($id);
+        }
+
+        $this->render('updateattribute' , array('attr' => $attr));
+
+    }
+
+    public function actionDeleteTitle(){
+        if(!Yii::app()->request->isAjaxRequest) throw new CHttpException(404,'Такой страницы не существует!');
+
+        $id = CHtml::encode($_POST['id']);
+        $model = SAttributeTitles::model()->findByPk($id);
+
+//        удалим атрибут у всех продуктов
+        SAttrs::model()->deleteAll('attribute_id = :id' , array(':id' => $id));
+        SAttrvalue::model()->deleteAll('attr_id = :id' , array(':id' => $id));
+
+//      расставим позиции
+        $positions_over = SAttributeTitles::model()->findAll(array(
+            'condition'=>'category_id = :category_id AND pos > :pos',
+            'params' => array(
+                ':category_id' => $model->category_id,
+                ':pos' => $model->pos,
+            ),
+        ));
+        if($positions_over !== null) {
+            foreach($positions_over as $po){
+                $po->pos = $po->pos - 1;
+                $po->save();
+            }
+        }
+
+//      удаление записи
+        $model->delete();
+    }
+
+    public function actionUpdatePositions(){
+        if(!Yii::app()->request->isAjaxRequest) throw new CHttpException(404,'Такой страницы не существует!');
+        $positions = array();
+        parse_str(Yii::app()->request->getPost('positions'),$positions);
+        foreach ($positions['item'] as $key => $value){
+            $model = SAttributeTitles::model()->findByPk(intval($value));
+            if($model!==null){
+                $model->pos = $key;
+                $model->save();
+            }
+        }
+    }
+
+
+    public function loadTitlesModel($id)
     {
-        $model=SBrands::model()->findByPk($id);
+        $model=SAttributeTitles::model()->findByPk($id);
         if($model===null)
             throw new CHttpException(404,'Извините, такой страницы не существует');
         return $model;
